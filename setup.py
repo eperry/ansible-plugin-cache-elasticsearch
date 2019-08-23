84ad269806a847a839c4560c77e92d5ee35b9d66
Your Name <you@example.com>
2019-08-23 13:14:11 -0400

    update

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='plugins.cache.elasticsearch',
+      version='0.1',
+      description='Elasticsearch Cache Plugin',
+      url='git@github.com:eperry/ansible-plugin-cache-elasticsearch.git',
+      author='Edward Perry',
+      author_email='edwardperry1@gmail.com',
+      packages=['plugins.cache.elasticsearch'],
+      install_requires=[
+          'elasticsearch',
+          'pytz',
+           'json',
+           'logging',
+           'pytz',
+           'time',
+           'json'
+      ],
+      zip_safe=False)
